08b7915a1849d64a9dff93ba1213cf400bcbfd73
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-02-06 23:37:51 +0600

    added answer

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 1.first 
+// 1.First 
 // first get input positive number and second this input multiplyied by 3. Then this number operation by 10. And then this number divide by 2 and last this number substraction by 5.
 
 function mindGame(number){
@@ -16,12 +16,11 @@ function mindGame(number){
 }
 const input = 5;
 const total = mindGame(input);
-// console.log(total);
+console.log(total);
 
 
-// second
+// Second
 //first, get a string input. then there how much character here, find this. if totalcharacers are even, return "even". if totalcharacers are odd, return "odd".
-
 function evenOdd(str){
     if(typeof str !== 'string'){
         return 'Inter a string';
@@ -38,7 +37,21 @@ const totalCharacers = evenOdd(myName);
 // console.log(totalCharacers);
 
 
-
-
-
+// Third
+// first get a input number. then the input substracted by 7. if result less then 7, return the result number. if result greaterthen 7, return the inputs double.
+function isLGSeven(number){
+let numberDistance = number - 7;
+if(typeof number !== 'number'){
+    return 'Input should be a number';
+}
+else if(numberDistance < 7){
+    return numberDistance;
+}
+else if(numberDistance >= 7){
+    return number * 2;
+}
+}
+const number = 15;
+const totalResult = isLGSeven(number);
+// console.log(totalResult);
 
