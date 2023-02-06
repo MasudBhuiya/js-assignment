2d6b87e030878b36d1acc65d7be99521dcb5f5d5
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-02-07 00:18:34 +0600

    added four

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ function mindGame(number){
 }
 const input = 5;
 const total = mindGame(input);
-console.log(total);
+// console.log(total);
 
 
 // Second
 //first, get a string input. then there how much character here, find this. if totalcharacers are even, return "even". if totalcharacers are odd, return "odd".
 function evenOdd(str){
     if(typeof str !== 'string'){
-        return 'Inter a string';
+        return 'Input should be a string';
     }
     else if(str.length % 2 === 0){
         return 'Even';
@@ -38,7 +38,7 @@ const totalCharacers = evenOdd(myName);
 
 
 // Third
-// first get a input number. then the input substracted by 7. if result less then 7, return the result number. if result greaterthen 7, return the inputs double.
+// first, get a input number. then the input substracted by 7. if result less then 7, return the result number. if result greaterthen 7, return the inputs double.
 function isLGSeven(number){
 let numberDistance = number - 7;
 if(typeof number !== 'number'){
@@ -54,4 +54,61 @@ else if(numberDistance >= 7){
 const number = 15;
 const totalResult = isLGSeven(number);
 // console.log(totalResult);
+
+
+//Four
+//first, get a input number. there are Good or Bad numbers in this array. search the bad numbers and return how much Bad numbers on this array.
+function findingBadData(arra){
+    let badNumbers = [];
+    if(Array.isArray(arra) !== true){
+        return 'Input should be an array';
+    }
+    for(let i = 0; i < arra.length; i++){
+        const number = arra[i];
+        if(number <= -1){
+            badNumbers.push(number);
+        }
+    }
+    return badNumbers.length;
+}
+const numbers = [12, 66, -54, -45, 67, -76, 56, 24, -56, -95];
+const badData = findingBadData(numbers);
+// console.log(badData);
+
+
+//five
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
